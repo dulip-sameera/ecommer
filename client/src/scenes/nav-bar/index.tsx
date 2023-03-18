bde3bc13afa3530a1b6bd672a21ea94a1e0243a6
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-18 15:56:26 +0530

    feat: create navbar component

--- client/src/scenes/nav-bar/index.tsx
+++ client/src/scenes/nav-bar/index.tsx
@@ -1,7 +1,58 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { UserIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useDispatch } from "react-redux/es/exports";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import "./nav-bar.styles.css";
+import { RootState } from "@/state/store";
+import { setIsCartOpen } from "@/state/cart.slice";
+
 type Props = {};
 
 const NavBar = (props: Props) => {
-  return <div>NavBar</div>;
+  const dispatch = useDispatch();
+  const cart = useSelector((state: RootState) => state.cart.cart);
+
+  return (
+    <div className="navbar__container">
+      <nav className="navbar">
+        {/* BRAND LOGO */}
+        <div className="navbar__logo">ECOMMER</div>
+
+        {/* LINKS */}
+        <div className="navbar__links">
+          {/* SEARCH */}
+          <button className="navbar__link">
+            <MagnifyingGlassIcon className="navbar__link--icon" />
+          </button>
+          {/* ACCOUNT */}
+          <button className="navbar__link">
+            <UserIcon className="navbar__link--icon" />
+          </button>
+          {/* SHOPPING CART */}
+          <button
+            className="navbar__link navbar__link--cart"
+            onClick={() => dispatch(setIsCartOpen(true))}
+          >
+            {/* DON'T SHOW IF CART IS EMPTY */}
+            <span
+              className={
+                cart.length > 0 ? "cart__badge" : "cart__badge--hidden"
+              }
+            >
+              {cart.length}
+            </span>
+            <ShoppingBagIcon className="navbar__link--icon" />
+          </button>
+          {/* MENU */}
+          <button className="navbar__link">
+            <Bars3Icon className="navbar__link--icon" />
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
