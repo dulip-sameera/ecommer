9e3ec2fc46a8be3f71953decd4d34ccc1be6a96b
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-20 20:50:15 +0530

    feat: change the Item type

--- client/src/shared/types.ts
+++ client/src/shared/types.ts
@@ -1,10 +1,30 @@
 export type Item = {
-  id: string;
-  shortDescription: string;
-  longDescription: string;
-  price: number;
-  image: string;
-  category: string;
+  id: number;
+  attributes: {
+    name: string;
+    price: number;
+    longDescription: string;
+    shortDescription: string;
+    category: string;
+    image: {
+      data: {
+        id: number;
+        attributes: {
+          formats: {
+            medium: {
+              url: string;
+            };
+            small: {
+              url: string;
+            };
+            thumbnail: {
+              url: string;
+            };
+          };
+        };
+      };
+    };
+  };
 };
 
 export type CartItem = {
