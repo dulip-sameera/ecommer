246a1108c81f30745f579bbc9977fac86e14a762
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-20 20:50:37 +0530

    feat: add products list section

--- client/src/scenes/home/Products.tsx
+++ client/src/scenes/home/Products.tsx
@@ -1,17 +1,56 @@
+import Product from "@/components/Product";
+import { useProducts } from "@/hook/useProducts";
 import { useState } from "react";
 import "./products.styles.css";
 
 enum TABS {
-  ALL = "ALL",
-  NEW_ARRIVALS = "NEW ARRIVALS",
-  BEST_SELLERS = "BEST SELLERS",
-  TOP_RATED = "TOP RATED",
+  ALL = "all",
+  NEW_ARRIVALS = "new-arrivals",
+  BEST_SELLERS = "best-sellers",
+  TOP_RATED = "top-rated",
 }
 
 type Props = {};
 
 const Products = (props: Props) => {
+  const { data, isError, isLoading, error } = useProducts();
   const [currentTab, setCurrentTab] = useState(TABS.ALL);
+
+  if (isLoading) {
+    return (
+      <section className="products">
+        <div> Loading...</div>
+      </section>
+    );
+  }
+
+  if (isError) {
+    console.log("🚀 ~ PRODUCT FETCHING ERROR:", error);
+    return (
+      <section className="products">
+        <div>Something Went Wrong</div>
+        <div>{error.message}</div>
+      </section>
+    );
+  }
+
+  // ALl
+  const allProducts = data.data;
+
+  // NEW ARRIVALS
+  const newArrivals = data.data.filter(
+    (product) => product.attributes.category === TABS.NEW_ARRIVALS
+  );
+
+  // BEST SELLERS
+  const bestSellers = data.data.filter(
+    (product) => product.attributes.category === TABS.BEST_SELLERS
+  );
+
+  // TOP RATED
+  const topRated = data.data.filter(
+    (product) => product.attributes.category === TABS.TOP_RATED
+  );
 
   return (
     <section className="products">
@@ -29,7 +68,7 @@ const Products = (props: Props) => {
               currentTab === TABS.ALL ? "products__tab--selected" : ""
             } products__tab`}
           >
-            <button onClick={() => setCurrentTab(TABS.ALL)}>{TABS.ALL}</button>
+            <button onClick={() => setCurrentTab(TABS.ALL)}>ALL</button>
           </li>
           <li
             className={`${
@@ -37,7 +76,7 @@ const Products = (props: Props) => {
             } products__tab`}
           >
             <button onClick={() => setCurrentTab(TABS.NEW_ARRIVALS)}>
-              {TABS.NEW_ARRIVALS}
+              NEW ARRIVALS
             </button>
           </li>
           <li
@@ -46,7 +85,7 @@ const Products = (props: Props) => {
             } products__tab`}
           >
             <button onClick={() => setCurrentTab(TABS.BEST_SELLERS)}>
-              {TABS.BEST_SELLERS}
+              BEST SELLERS
             </button>
           </li>
           <li
@@ -55,14 +94,34 @@ const Products = (props: Props) => {
             } products__tab`}
           >
             <button onClick={() => setCurrentTab(TABS.TOP_RATED)}>
-              {TABS.TOP_RATED}
+              TOP RATED
             </button>
           </li>
         </ul>
       </div>
 
       {/* PRODUCT LIST */}
-      <div className="products__list"></div>
+      <section className="products__list">
+        {currentTab === TABS.ALL &&
+          allProducts.map((product) => (
+            <Product product={product} key={`${product.id}`} />
+          ))}
+
+        {currentTab === TABS.NEW_ARRIVALS &&
+          newArrivals.map((product) => (
+            <Product product={product} key={`${product.id}`} />
+          ))}
+
+        {currentTab === TABS.BEST_SELLERS &&
+          bestSellers.map((product) => (
+            <Product product={product} key={`${product.id}`} />
+          ))}
+
+        {currentTab === TABS.TOP_RATED &&
+          topRated.map((product) => (
+            <Product product={product} key={`${product.id}`} />
+          ))}
+      </section>
     </section>
   );
 };
