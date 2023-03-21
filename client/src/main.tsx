d735bdc80103eeecc899dafde8208405d64f86db
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-21 11:29:10 +0530

    feat: add browser router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { store } from "./state/store";
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>
