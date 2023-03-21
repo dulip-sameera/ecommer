3a4d6b674dd5daf4117385c8327359741936cc9b
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-21 10:13:59 +0530

    feat: add routing to home and single product paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import NavBar from "@/scenes/nav-bar";
+import Home from "@/scenes/home";
+import Footer from "@/scenes/footer";
+import Cart from "@/scenes/cart/Cart";
+import { Routes, Route } from "react-router-dom";
+import SingleProduct from "@/scenes/single-product";
 
 type Props = {};
 
@@ -7,6 +12,12 @@ const App = (props: Props) => {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+      <Cart />
+      <Footer />
     </div>
   );
 };
