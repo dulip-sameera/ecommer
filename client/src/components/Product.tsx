057aa691a852f90f8c0c20df4f00ad94305097fc
Dulip Sameeera <dsgitrepo101@gmail.com>
2023-03-21 08:09:32 +0530

    feat: add hovering buttons

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,5 +1,10 @@
 import { Item } from "@/shared/types";
-import "./products.styles.css";
+import { addToCart, decreaseCount, increaseCount } from "@/state/cart.slice";
+import { RootState } from "@/state/store";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./product.styles.css";
 
 type Props = {
   product: Item;
@@ -8,6 +13,12 @@ type Props = {
 const BASE_URL = "http://localhost:1337";
 
 const Product = ({ product }: Props) => {
+  const cartItems = useSelector((state: RootState) => state.cart.cart);
+  const dispatch = useDispatch();
+  const [hover, setHover] = useState(false);
+
+  const productInCart = cartItems.find((item) => item.item.id === product.id);
+
   return (
     <div className="product__card">
       <div className="product__card--image-container">
@@ -16,6 +27,35 @@ const Product = ({ product }: Props) => {
           src={`${BASE_URL}${product.attributes.image.data.attributes.formats.medium.url}`}
           alt={`${product.attributes.name}`}
         />
+        <div className="product__card--buttons">
+          <div className="product__changecount">
+            <button
+              className="product__changecount--increase"
+              onClick={() => dispatch(decreaseCount(product.id))}
+            >
+              <MinusIcon className="changecount--icon" />
+            </button>
+            <span className="product__changecount--count">
+              {productInCart ? productInCart.count : 0}
+            </span>
+
+            <button
+              className="product__changecount--decrease"
+              onClick={() => dispatch(increaseCount(product.id))}
+            >
+              <PlusIcon className="changecount--icon" />
+            </button>
+          </div>
+
+          <div className="product__addtocart">
+            <button
+              className="product__addtocart--button"
+              onClick={() => dispatch(addToCart(product))}
+            >
+              ADD TO CART
+            </button>
+          </div>
+        </div>
       </div>
       <div className="product__card--body">
         <h4 className="product__card--body-heading">
